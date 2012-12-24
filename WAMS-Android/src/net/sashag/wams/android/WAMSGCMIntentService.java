34ee8d435b0717143e88ee7f7d429df70c20bb43
Sasha Goldshtein <goldshtn@gmail.com>
2012-12-24 11:57:20 +0200

    Fixed imports

--- WAMS-Android/src/net/sashag/wams/android/WAMSGCMIntentService.java
+++ WAMS-Android/src/net/sashag/wams/android/WAMSGCMIntentService.java
@@ -7,12 +7,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Handler;
-import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
 
